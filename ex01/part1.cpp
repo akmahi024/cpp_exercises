a7cbb8e50cb3501a4401394acc6a89e6e7c2a55e
akmahi024 <145624870+akmahi024@users.noreply.github.com>
2024-10-03 12:13:24 +0900

    Create part1.cpp

---
+++ ex01/part1.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <stdbool.h>
+#include <assert.h>
+
+bool isPrime(unsigned int x){
+    if(x<2){
+        return false;
+    }
+    for(unsigned int i =2;i*i<=x;i++){
+        if(x%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    printf("%d:%s\n",60,isPrime(60) ? "true" : "false");
+    printf("%d:%s\n",50,isPrime(50) ? "true" : "false");
+    printf("%d:%s\n",1,isPrime(1) ? "true" : "false");
+    printf("%d:%s\n",5,isPrime(5) ? "true" : "false");
+    printf("%d:%s\n",500,isPrime(500) ? "true" : "false");
+}

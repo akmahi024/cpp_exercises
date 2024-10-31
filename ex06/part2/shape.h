d5791520eaeb41d75e23c1ba4fc1b411aa502f8f
ABDUL KADER MAHI <145624870+akmahi024@users.noreply.github.com>
2024-10-31 14:40:59 +0900

    no change

--- ex06/part2/shape.h
+++ ex06/part2/shape.h
@@ -1,25 +1,26 @@
+// shape.h
+
 #ifndef SHAPE_H
 #define SHAPE_H
 
 #include <string>
 
-// Interface definition for Shape
 class Shape {
 public:
-    // Pure virtual function to get the name of the shape
+    // Returns the name of the shape
     virtual std::string get_name() const = 0;
 
-    // Pure virtual function to compute the area of the shape
+    // Computes the area of the shape
     virtual float compute_area() const = 0;
 
-    // Pure virtual function to create a new object using the default constructor
+    // Creates a new object using the default constructor
     virtual Shape* create() const = 0;
 
-    // Pure virtual function to clone the object using the copy constructor
+    // Clones the current object using the copy constructor
     virtual Shape* clone() const = 0;
 
-    // Virtual destructor to allow proper cleanup for derived classes
-    virtual ~Shape() {}
+    // Virtual destructor
+    virtual ~Shape() = default;
 };
 
 #endif // SHAPE_H

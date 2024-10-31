74fe575f6f99d0b342183579d92c7b5475f56d16
ABDUL KADER MAHI <145624870+akmahi024@users.noreply.github.com>
2024-10-31 16:39:20 +0900

    Update disk.h

--- ex06/disk.h
+++ ex06/disk.h
@@ -1,37 +1,26 @@
+// disk.h
+
 #ifndef DISK_H
 #define DISK_H
 
 #include "shape.h"
-#include <cmath>  // For M_PI
+#include "point.h"
 #include <string>
 
-// Point structure representing a point in 2D space
-struct Point {
-    float x, y;
-    Point(float x_val = 0.0f, float y_val = 0.0f) : x(x_val), y(y_val) {}
-};
-
-// Disk class implementing the Shape interface
 class Disk : public Shape {
-private:
-    Point center;
-    float radius;
-
 public:
-    // Constructor
     Disk(Point center, float radius);
 
-    // Copy constructor (for clone method)
-    Disk(const Disk& other);
-
-    // Overriding Shape interface methods
     std::string get_name() const override;
     float compute_area() const override;
     Disk* create() const override;
     Disk* clone() const override;
 
-    // Destructor
-    ~Disk() override {}
+    ~Disk() override = default;
+
+private:
+    Point center_;
+    float radius_;
 };
 
 #endif // DISK_H
